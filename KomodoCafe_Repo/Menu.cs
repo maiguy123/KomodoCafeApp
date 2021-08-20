2a84bffc78f67464049d3d98afeac79fa6464320
John Mai <Maijohn960@gmail.com>
2021-08-20 11:02:25 -0400

    Add project files.

---
+++ KomodoCafe_Repo/Menu.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GoldBadgeChallenge
+{
+  public class  Menu
+    {
+        public Menu () {  }
+        public Menu(string NameOfMeal, string Description, string Ingredients, string Price)
+        {
+            NameOfMeal = nameofmeal;
+            Description = descripition;
+            Ingredients = ingredients;
+            Price = price;
+        }
+         public string nameofmeal { get; set; } 
+         public string descripition { get; set; } 
+         public string ingredients { get; set; } 
+         public string price { get; set; } 
+
+
+    }
+ 
+
+
+}
+
+
+
+
+
+
+
---
+++ KomodoCafe_Repo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GoldBadgeChallenge")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GoldBadgeChallenge")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8af6d75d-5a6c-4026-8018-0fd1a29bda35")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
